16b41983a80793d5b61ea30674a6bcfa75baf752
Alexandre SALMON <salmon.alexandre10@gmail.com>
2019-12-11 12:31:55 +0100

    done + deconnexion

--- src/server.c
+++ src/server.c
@@ -12,12 +12,42 @@
 
 #define SERVICE_DEFAUT "1111"
 #define SERVEUR_DEFAUT "127.0.0.1"
+#define SIZE_REV 255
 
 struct server_instance {
+	struct sockaddr_in *p_adr_serv;
 	int socket;
 	array_list_t *clients;
 	int max_socket;
 };
+
+struct server_instance *create_instance(int socket, struct sockaddr_in *p_adr_serv) {
+	struct server_instance *instance = malloc(sizeof(struct server_instance));
+	instance->p_adr_serv = p_adr_serv;
+	instance->socket = socket;
+	instance->clients = init_array_list();
+	instance->max_socket = socket;
+	return instance;
+}
+
+int add_client(struct server_instance *instance) {
+	int new_sock = h_accept(instance->socket, instance->p_adr_serv);
+	add_element(instance->clients, (void *) new_sock);
+	instance->max_socket = new_sock > instance->max_socket ? new_sock : instance->max_socket;
+	printf("new socket %d count %d\n", new_sock, instance->clients->current_index);
+	return new_sock;
+}
+
+void remove_client(struct server_instance *instance, int socket) {
+	remove_element(instance->clients, (void *) socket);
+	h_close(socket);
+	printf("lost socket %d count %d\n", socket, instance->clients->current_index);
+	if (instance->max_socket == socket) {
+		int max = 0;
+		for (int i = 0; i < instance->clients->current_index; ++i)
+			max = max > (int) get(instance->clients, i) ? max : (int) get(instance->clients, i);
+	}
+}
 
 void serveur_appli(char *service)
 /* procedure correspondant au traitement du client de votre application */
@@ -25,26 +55,19 @@ void serveur_appli(char *service)
 	printf("%s\n", service);
 
   struct sockaddr_in *p_adr_serv;
-	int numsockserveur = h_socket(AF_INET, SOCK_STREAM);
+	int sockServeur = h_socket(AF_INET, SOCK_STREAM);
 	adr_socket(SERVICE_DEFAUT, SERVEUR_DEFAUT, SOCK_STREAM, &p_adr_serv);
 
-	h_bind(numsockserveur, p_adr_serv);
-	h_listen(numsockserveur, 1);
+	h_bind(sockServeur, p_adr_serv);
+	h_listen(sockServeur, 1);
 
-  char *str_recv = malloc(sizeof(char) * 5);
+  char *str_recv = malloc(sizeof(char) * SIZE_REV);
 	fd_set rdfs;
 
-	// int nb_clients = 0;
-	// int max_clients = 5;
-	// int max_socket = numsockserveur;
-	// int *clients = malloc(sizeof(int) * max_clients);
-
-	struct server_instance *instance = malloc(sizeof(struct server_instance));
-	instance->socket = numsockserveur;
-	instance->clients = init_array_list();
-	instance->max_socket = numsockserveur;
+	struct server_instance *instance = create_instance(sockServeur, p_adr_serv);
 
 	while(1) {
+		printf("loop\n");
 		FD_ZERO(&rdfs);
 		FD_SET(instance->socket, &rdfs);
 
@@ -56,29 +79,26 @@ void serveur_appli(char *service)
 	    exit(errno);
 	  }
 		if (FD_ISSET(instance->socket, &rdfs)) {
-			int new_sock = h_accept(instance->socket, p_adr_serv);
-			add_element(instance->clients, (void *) new_sock);
-			instance->max_socket = new_sock > instance->max_socket ? new_sock : instance->max_socket;
-			printf("nb_clients: %d\n", instance->clients->current_index);
+			printf("listening server\n");
+			int new_sock = add_client(instance);
 			h_writes(new_sock, "ping", 5);
 		} else {
+			printf("listening clients\n");
 			for (int i = 0; i < instance->clients->current_index; ++i) {
 				if (FD_ISSET((int) get(instance->clients, i), &rdfs)) {
-					h_reads(get(instance->clients, i), str_recv, 5);
-					printf("%s\n", str_recv);
-					char *buff = malloc(sizeof(char) * 5);
-					sprintf(buff, "%d", i);
-					h_writes(get(instance->clients, i), buff, 5);
+					int ret = h_reads(get(instance->clients, i), str_recv, 5);
+					if (ret > 0) {
+						printf("%s\n", str_recv);
+						char *buff = malloc(sizeof(char) * 5);
+						sprintf(buff, "%d", i);
+						h_writes(get(instance->clients, i), buff, 5);
+					} else {
+						remove_client(instance, (int) get(instance->clients, i));
+					}
 				}
 			}
 		}
 	}
-
-	// do {
-	// 	h_writes(numsockclient, "ping", 5);	//On envoi le mot à trouver (avec les tirets)
-	// 	h_reads(numsockclient, str_recv, 5);	// On récupère la saisie du client
-  //   printf("%s\n", str_recv);
-	// } while (1);
 }
 
 int main(int argc, char *argv[])
