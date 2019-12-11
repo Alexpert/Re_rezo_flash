f2f92012654868017390e187fe5da212bbe621ba
Alexandre SALMON <salmon.alexandre10@gmail.com>
2019-12-11 08:41:47 +0100

    basic multi

--- src/server.c
+++ src/server.c
@@ -26,26 +26,41 @@ void serveur_appli(char *service)
 	h_bind(numsockserveur, p_adr_serv);
 	h_listen(numsockserveur, 1);
 
-	int numsockclient = h_accept(numsockserveur, p_adr_serv);
-	h_writes(numsockclient, "ping", 5);
   char *str_recv = malloc(sizeof(char) * 5);
-
 	fd_set rdfs;
+
+	int nb_clients = 0;
+	int max_clients = 5;
+	int max_socket = numsockserveur;
+	int *clients = malloc(sizeof(int) * max_clients);
 
 	while(1) {
 		FD_ZERO(&rdfs);
-		FD_SET(numsockclient, &rdfs);
+		FD_SET(numsockserveur, &rdfs);
+		for (int i = 0; i < nb_clients; ++i) {
+			FD_SET(clients[i], &rdfs);
+		}
 
-	  if((select(numsockclient + 1, &rdfs, NULL, NULL, NULL)) < 0) {
+	  if((select(max_socket + 1, &rdfs, NULL, NULL, NULL)) < 0) {
 			perror("select()");
 	    exit(errno);
 	  }
-
-		if (FD_ISSET(numsockclient, &rdfs)) {
-			h_reads(numsockclient, str_recv, 5);
-			printf("%s\n", str_recv);
+		if (FD_ISSET(numsockserveur, &rdfs)) {
+			int numsockclient = h_accept(numsockserveur, p_adr_serv);
+			clients[nb_clients++] = numsockclient;
+			max_socket = numsockclient > max_socket ? numsockclient : max_socket;
+			printf("nb_clients: %d\n", nb_clients);
 			h_writes(numsockclient, "ping", 5);
-	    sleep(1);
+		} else {
+			for (int i = 0; i < nb_clients; ++i) {
+				if (FD_ISSET(clients[i], &rdfs)) {
+					h_reads(clients[i], str_recv, 5);
+					printf("%s\n", str_recv);
+					char *buff = malloc(sizeof(char) * 5);
+					sprintf(buff, "%d", i);
+					h_writes(clients[i], buff, 5);
+				}
+			}
 		}
 	}
 
